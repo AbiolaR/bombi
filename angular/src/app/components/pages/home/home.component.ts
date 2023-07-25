27c07106857cc646692873ee65461118886a4a9e
Abiola Rasheed <rasheedabi97@hotmail.com>
2023-07-25 23:20:58 +0200

    added profile button to home page

--- angular/src/app/components/pages/home/home.component.ts
+++ angular/src/app/components/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserService } from 'src/app/services/user.service';
+import { ProfileDialogComponent } from '../../dialogs/profile-dialog/profile-dialog.component';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  constructor() { }
+  constructor(public userService: UserService, private dialog: MatDialog) { }
+
+  openProfileDialog() {
+    this.dialog.open(ProfileDialogComponent, {
+      width: '500px',
+      autoFocus: false
+    });
+  }
 }
+
