7b240731b58f1b45c35430ec519c33fca1369645
Abiola Rasheed <rasheedabi97@hotmail.com>
2023-11-06 23:39:18 +0100

    changed some styling on book

--- angular/src/app/components/message/message.component.ts
+++ angular/src/app/components/message/message.component.ts
@@ -23,11 +23,13 @@ export class MessageComponent implements OnInit {
     if (this.message) {
       this.message.message = this.message.message?.replaceAll('\n', '<br/>');
 
-      if (!this.message.coverUrl) {
+      if (!this.message.coverUrl || this.message.coverUrl.includes('blank.png')) {
         this.coverUrl = '/assets/images/covers/blank.png';
       } else if (this.message.coverUrl.startsWith('https://books.google.com/')
         || this.message.coverUrl.startsWith('http://books.google.com/')) {
         this.coverUrl = this.message.coverUrl;
+      } else if (this.message.coverUrl.includes('/fictioncovers/')) {
+        this.coverUrl = 'https://libgen.li' + this.message.coverUrl;
       } else {
         this.coverUrl = environment.apiServerUrl + '/v1/books/fictioncovers/' + this.message.coverUrl
       }
