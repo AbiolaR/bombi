3ec297450c6a5106c37c68aa0fc2fe7890dfabf3
Abiola Rasheed <rasheedabi97@hotmail.com>
2023-11-11 15:14:00 +0100

    updated readme; added docker versioning with tags

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # bombi
 Mixture of Ombi and Readarr for books
 
-Acts as a kind of rapper of https://libgen.li to make downloading books more user friendly.
-Enables the user to send books to a konfigured E-Reader device with the click of a button.
+Enables the user to send books to a configured E-Reader device with the click of a button.
 
 Can be found at https://bombi.tinym.de
-
-WIP: Syncinc books from Social Reading Platforms like Goodreads or The Storygraph to your konfigured E-Reader device.
--- deployImage.py
+++ deployImage.py
@@ -1,9 +1,29 @@
 import os
+import keyring
+import requests
+
+registry = 'docker.tinym.de'
+appName = 'bombi'
+
+credential = keyring.get_credential(registry, '')
+
+session = requests.Session()
+session.auth = (credential.username, credential.password)
+
+response = session.get(f'https://{registry}/v2/{appName}/tags/list')
+
+version = open('release-version.txt', 'r').read()
+
+if version in response.json()['tags']:
+    print('Version already exists!')
+    quit()
 
 os.chdir('./angular')
 os.system('npm link @angular/cli')
 os.system('ng build')
 os.chdir('..')
-os.system('docker login docker.tinym.de')
-os.system('docker build . -t docker.tinym.de/bombi')
-os.system('docker push docker.tinym.de/bombi')
+os.system(f'docker login {registry}')
+os.system(f'docker build -t {registry}/{appName} .')
+os.system(f'docker tag {registry}/{appName} {registry}/{appName}:{version}')
+os.system(f'docker push {registry}/{appName}')
+os.system(f'docker push {registry}/{appName}:{version}')
