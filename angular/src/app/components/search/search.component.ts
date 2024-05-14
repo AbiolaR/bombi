045e397e8a8be44d572c1614c484824740f7f820
AbiolaR <rasheedabi97@hotmail.com>
2024-05-15 00:01:32 +0200

    feat: :sparkles: now actuallt doing an autocomplete search on the entire search history

--- angular/src/app/components/search/search.component.ts
+++ angular/src/app/components/search/search.component.ts
@@ -5,6 +5,7 @@ import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
 import { MatInput } from '@angular/material/input';
 import { MatMenuTrigger } from '@angular/material/menu';
 import { Router } from '@angular/router';
+import { Observable, filter, map, startWith } from 'rxjs';
 import { LanguageMap } from 'src/app/models/language-map';
 import { UserData } from 'src/app/models/user-data';
 import { UserService } from 'src/app/services/user.service';
@@ -33,11 +34,11 @@ export class SearchComponent {
 
   constructor(private router: Router, private userService: UserService) { }
 
+  filteredSearchHistory: Observable<Map<string, string>> | undefined;
   minimumLength: number = 4;
   searchForm = new FormGroup({ 
                     input: new FormControl(this.searchString, 
-                      { validators: [Validators.minLength(this.minimumLength), this.noEmptyStringValidator], 
-                        updateOn: 'submit'})});
+                      { updateOn: 'change'})});
 
 
   ngOnInit() {
@@ -51,6 +52,7 @@ export class SearchComponent {
   }
 
   public onSubmit() {
+    this.validateForm();
     if (this.searchForm.valid) {
       this.saveToSearchHistory();
       (this.searchInput as any)._elementRef.nativeElement.blur();
@@ -72,6 +74,16 @@ export class SearchComponent {
     return (this.searchInput?.value.length || 0) > 0; 
   }
 
+  private validateForm() {
+    const value = this.searchForm.get('input')?.value || '';
+    if (value.length <= this.minimumLength || !Boolean(value.trim())) {
+      this.searchForm.get('input')?.setErrors({'invalid': true});
+    } else {
+      this.searchForm.get('input')?.setErrors({});
+      this.searchForm.get('input')?.updateValueAndValidity();
+    }
+  }
+
   private saveToSearchHistory() {
     if (this.userData.getNewestSearchHistoryEntry() == this.searchForm.get('input')?.value) {
       return;
@@ -86,6 +98,7 @@ export class SearchComponent {
   deleteSearchHistoryEntry(key: string, event: any) {
     this.userData.searchHistory.delete(key);
     this.saveSearchHistory();
+    this.searchForm.get('input')?.updateValueAndValidity();
     event.stopPropagation();
   }
 
@@ -106,6 +119,11 @@ export class SearchComponent {
           this.userData = response;
           this.userData.searchHistory = new Map(Object.entries(this.userData.searchHistory || new Map()));
           this.userData = Object.assign(new UserData, this.userData);
+
+          this.filteredSearchHistory = this.searchForm.get('input')?.valueChanges.pipe(      
+            startWith(''),
+            map(value => this._filter(value || '')),
+          );
         }
       });
     }
@@ -123,5 +141,19 @@ export class SearchComponent {
 
   keyDescOrder = (a: KeyValue<string,string>, b: KeyValue<string,string>): number => {
     return +a.key > +b.key ? -1 : (+b.key > +a.key ? 1 : 0);
+  }
+
+  private _filter(value: string): Map<string, string> {
+    if (!value) {
+      return this.userData.shortSearchHistory();
+    }
+    const filterValue = value.toLowerCase();
+    return new Map([...this.userData.searchHistory.entries()]
+      .filter(([_key, entry]) => entry.toLowerCase().includes(filterValue))
+      .sort(this.filterKeyDescOrder).slice(0,5));    
+  }
+
+  filterKeyDescOrder = (a: [string,string], b: [string,string]): number => {
+    return +a[0] > +b[0] ? -1 : (+b[0] > +a[0] ? 1 : 0);
   }
 }
